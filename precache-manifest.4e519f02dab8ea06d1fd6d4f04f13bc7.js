f474ea759e3bc5fad6c3513eb7bbc04ebb0deceb
Felipe Vieira <flpvieira86@gmail.com>
2020-09-06 14:48:17 -0300

    Updates

---
+++ precache-manifest.4e519f02dab8ea06d1fd6d4f04f13bc7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc21ceda66426ce438b444f24e911254",
+    "url": "/onlinestore/index.html"
+  },
+  {
+    "revision": "4e7efc2a52bff73450cf",
+    "url": "/onlinestore/static/css/main.ba67fec5.chunk.css"
+  },
+  {
+    "revision": "cf9b8a31edd5dbe0cd78",
+    "url": "/onlinestore/static/js/2.e840cb34.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/onlinestore/static/js/2.e840cb34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e7efc2a52bff73450cf",
+    "url": "/onlinestore/static/js/main.b1a5c870.chunk.js"
+  },
+  {
+    "revision": "9b42353b26afcd636f64",
+    "url": "/onlinestore/static/js/runtime-main.682b5f09.js"
+  },
+  {
+    "revision": "fa2da4e6b61aa745f198314584dd8826",
+    "url": "/onlinestore/static/media/search-icon.fa2da4e6.png"
+  },
+  {
+    "revision": "aa9925ad75ebd634a562304eac33712d",
+    "url": "/onlinestore/static/media/shopping-cart.aa9925ad.png"
+  }
+]);
